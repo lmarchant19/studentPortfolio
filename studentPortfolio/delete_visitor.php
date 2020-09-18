a7c64a20f7e3aacb3152ed51f563a3f64b3a79a9
lmarchant19 <60169092+lmarchant19@users.noreply.github.com>
2020-09-18 10:45:27 -0600

    Update delete_visitor.php

--- studentPortfolio/delete_visitor.php
+++ studentPortfolio/delete_visitor.php
@@ -6,7 +6,9 @@
 #Logan      9/11/2020     Added delete functionality
 #
 #*********************************************************************************
-require_once('database.php');
+
+require_once('./model/visitor.php');
+require_once('./model/database.php');
 
 // Get IDs
 $visitor_id = filter_input(INPUT_POST, 'visitor_id', FILTER_VALIDATE_INT);
@@ -17,13 +19,7 @@ echo "The visitor id: $visitor_id";
 
 // Delete the product from the database
 if ($visitor_id != false) {
-    echo "Inside the if statement.";
-    $query = 'DELETE FROM visitor
-              WHERE visitor_id = :visitor_id';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':visitor_id', $visitor_id);
-    $statement->execute();
-    $statement->closeCursor();    
+    delVisitor($visitor_id);
 }
 
 // Display the Product List page
