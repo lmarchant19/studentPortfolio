733596a0f2bebef83cd913243440d913914b9d0b
lmarchant19 <60169092+lmarchant19@users.noreply.github.com>
2020-09-25 11:43:04 -0600

    Update employee.php

--- studentPortfolio/model/employee.php
+++ studentPortfolio/model/employee.php
@@ -3,17 +3,21 @@
     /* Date        Name             Description                                               */
     /* ----------  ---------------- --------------------------------------------------------- */
     /* 9-18-2020    logan Marchant       Added new model for employee entity               */
-    /*                                                                                        */
+    /* 9-23-2020    logan Marchant       Added static call to getDB() and try/catch                               */
     /******************************************************************************************/
-function getEmployees(){
-    global $db;
-    $query = 'SELECT * FROM employee
-                           ORDER BY employeeID';
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $employees = $statement->fetchAll();
-    $statement->closeCursor();
-    
+function getEmployees() {
+    //global $db;
+    try {
+        $db = Database::getDB();
+        $query = 'SELECT * FROM employee     ORDER BY employeeID';
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $employees = $statement->fetchAll();
+        $statement->closeCursor();
+    } catch (PDOException $e) {
+        include('../model/database_error.php');
+    }
+
     return $employees;
 }
 ?>
